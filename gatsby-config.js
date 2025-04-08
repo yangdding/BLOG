343544993659fdc5ab8e939b3ed47c0dd3883b30
yangdding <wlsdud060905@naver.com>
2025-04-09 01:32:47 +0900

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       title: "양진영의 블로그",
       description: "개발자 양진영의 기술 블로그",
       author: "Yang Jin Young",
-      siteUrlhttps:"https://blog-git-master-yd1ngs-projects.vercel.app/",
+      siteUrl:"https://blog-git-master-yd1ngs-projects.vercel.app/",
       },
   plugins: [
     `gatsby-plugin-catch-links`,
